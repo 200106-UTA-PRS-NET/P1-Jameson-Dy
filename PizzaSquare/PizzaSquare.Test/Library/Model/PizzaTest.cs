958b3fb89f9dcf2ff7b5abad148789ce94c46e0a
Jameson Dy <dyjameson22@gmail.com>
2020-01-31 01:53:14 -0600

    unit testing done

---
+++ PizzaSquare/PizzaSquare.Test/Library/Model/PizzaTest.cs
@@ -0,0 +1,43 @@
+using PizzaSquare.Lib;
+using System;
+using Xunit;
+
+namespace PizzaSquare.Test
+{
+    public class PizzaTest
+    {
+        readonly Pizzas pizzas = new Pizzas();
+
+        [Fact]
+        public void PizzaName_NonEmptyValue_StoresCorrectly()
+        {
+            const string randomPizzaNameValue = "Vegetarian";
+            pizzas.Name = randomPizzaNameValue;
+            Assert.Equal(randomPizzaNameValue, pizzas.Name);
+        }
+
+        [Fact]
+        public void Cheese_EmptyPizzas_Null()
+        {
+            Assert.Null(pizzas.Cheese);
+        }
+
+        [Fact]
+        public void Crust_EmptyPizzas_Null()
+        {
+            Assert.Null(pizzas.Crust);
+        }
+
+        [Fact]
+        public void Sauce_EmptyPizzas_Null()
+        {
+            Assert.Null(pizzas.Sauce);
+        }
+
+        [Fact]
+        public void Size_EmptyPizzas_Null()
+        {
+            Assert.Null(pizzas.Size);
+        }
+    }
+}
